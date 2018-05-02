312cbc700708c9b51ca817bba741ace33e13fcee
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-02 13:32:05 +1000

    Add helm-occur to leader

--- key-bindings/leader.el
+++ key-bindings/leader.el
@@ -15,6 +15,14 @@
 
 (define-key my-leader-map "p" 'evil-prev-buffer)
 
+; Start a Helm search in the current buffer.
+; Requires at least 2 characters.
+;
+; - [Helm: search within buffer feature? @ Emacs Stack Exchange](https://emacs.stackexchange.com/questions/2563/helm-search-within-buffer-feature)
+; - [Occur Mode @ Emacs wiki](https://www.emacswiki.org/emacs/OccurMode)
+;
+(define-key my-leader-map "s" 'helm-occur)
+
 ; Duplicates the `C-w` key-map.
 (define-key my-leader-map "w" 'evil-window-map)
 
