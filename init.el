35ae29cc6782257cbf2e0057090e6f142ea56631
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-03 12:59:52 +1000

    Reorder packages

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
 		  ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
 	  (quote
-		  (auto-complete yaml-mode helm helm-ebdb evil solarized-theme)))
+		  (auto-complete evil helm helm-ebdb solarized-theme yaml-mode)))
  '(tab-width 4))
 
 (custom-set-faces
