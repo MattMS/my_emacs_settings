c967fd6202bf170eae05f50b562187092438a49d
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-05 13:36:04 +1000

    Add auto-complete config

---
+++ my/auto-complete.el
@@ -0,0 +1,10 @@
+; ## Install
+;
+; Melpa: `M-x package-install [RET] auto-complete [RET]`
+;
+; - [Emacs wiki page](https://www.emacswiki.org/emacs/AutoComplete)
+; - [GitHub repo](https://github.com/auto-complete/auto-complete)
+;
+; ## Default configuration
+;
+(ac-config-default)
