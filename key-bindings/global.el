8d10649cb3b4cc0fc65446393c1fab7437fde198
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-05 13:38:04 +1000

    Update C-q and add C-S-q

--- key-bindings/global.el
+++ key-bindings/global.el
@@ -1,6 +1,12 @@
 (global-set-key (kbd "C-h a") 'helm-apropos)
 
-(global-set-key (kbd "C-q") 'save-buffers-kill-terminal)
+; Evil defaults `C-n` to `evil-paste-pop-next`.
+; Need to define evil-normal-state-map otherwise it will not work everywhere.
+;(define-key evil-normal-state-map (kbd "C-t") 'new-frame)
+;(global-set-key (kbd "C-t") 'new-frame)
+
+(global-set-key (kbd "C-q") 'delete-frame)
+(global-set-key (kbd "C-S-q") 'save-buffers-kill-terminal)
 
 ;(global-set-key (kbd "C-x") ctl-x-map)
 (global-set-key (kbd "M-x") 'helm-M-x)
