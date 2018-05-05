55854320b9c3cf9b956d1e4181df1ddeeb257cd0
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-05 13:37:12 +1000

    Add C-i to select action for files in Helm

--- key-bindings/helm.el
+++ key-bindings/helm.el
@@ -2,5 +2,7 @@
 
 (define-key helm-find-files-map (kbd "TAB") 'helm-ff-RET)
 
+(define-key helm-find-files-map (kbd "C-i") 'helm-select-action)
+
 ; Default for `TAB` is `helm-select-action`.
 ;(define-key helm-map (kbd "TAB") 'helm-select-action)
