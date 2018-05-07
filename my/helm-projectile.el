3f4503814f39ed1107372618a9455d93bde5890f
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-07 12:58:11 +1000

    Add projectile settings

---
+++ my/helm-projectile.el
@@ -0,0 +1,13 @@
+; ## Main links
+;
+; - [GitHub](https://github.com/bbatsov/helm-projectile)
+;
+; ---
+
+;(require 'helm-projectile)
+
+; Turn key-bindings on.
+(helm-projectile-on)
+
+; Turn key-bindings off.
+;(helm-projectile-off)
---
+++ my/projectile.el
@@ -0,0 +1,15 @@
+; ## Main links
+;
+; [Project default key-bindings](https://projectile.readthedocs.io/en/latest/usage/)
+;
+; ## Install
+;
+; `M-x package-install RET projectile RET`
+;
+; ---
+
+(projectile-mode)
+
+;(setq projectile-keymap-prefix (kbd "C-c C-p"))
+
+;(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
