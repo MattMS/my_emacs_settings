344b016d1aec6beed3c51c7d5dcd30383e70af91
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-07 12:26:29 +1000

    Restructure Helm key-bindings

--- key-bindings/helm.el
+++ key-bindings/helm.el
@@ -1,8 +1,8 @@
+; Default for `TAB` is `helm-select-action`.
+(define-key helm-map (kbd "TAB") nil)
+
 (define-key helm-buffer-map (kbd "TAB") 'helm-maybe-exit-minibuffer)
+(define-key helm-buffer-map (kbd "C-i") 'helm-select-action)
 
 (define-key helm-find-files-map (kbd "TAB") 'helm-ff-RET)
-
 (define-key helm-find-files-map (kbd "C-i") 'helm-select-action)
-
-; Default for `TAB` is `helm-select-action`.
-;(define-key helm-map (kbd "TAB") 'helm-select-action)
