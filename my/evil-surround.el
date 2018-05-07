def6a3b2c4b8c5032e87d3c4a5e11b39e6fe0fc1
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-07 12:26:05 +1000

    Add evil-surround

---
+++ my/evil-surround.el
@@ -0,0 +1,15 @@
+; ## Main links
+;
+; - [GitHub](https://github.com/emacs-evil/evil-surround)
+;
+; ## Key-bindings
+;
+; - `vi)` select everything between the brackets.
+; - `yi)` copy everything between the brackets.
+;
+; ---
+
+; Do not need to require when using `package-initialize`.
+;(require 'evil-surround)
+
+(global-evil-surround-mode 1)
