41f401d675990ae78a1ca5ea399371d29a77efe5
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-24 14:26:50 +1000

    Use glasses-mode on Python

--- mode-hooks/python.el
+++ mode-hooks/python.el
@@ -1,9 +1,12 @@
-; https://stackoverflow.com/questions/3684738/how-do-i-set-emacs-tab-size-to-4-chars-wide-for-py-files
-(add-hook
-	'python-mode-hook
-	(lambda ()
-		(setq indent-tabs-mode t)
-		(setq tab-width 4)
-		(setq python-indent 4)
-	)
+(defun my-python-mode-hook ()
+	(glasses-mode)
+
+	; https://emacs.stackexchange.com/questions/9583/how-to-treat-underscore-as-part-of-the-word
+	(modify-syntax-entry ?_ "w")
+
+	; https://stackoverflow.com/questions/3684738/how-do-i-set-emacs-tab-size-to-4-chars-wide-for-py-files
+	(setq indent-tabs-mode t)
+	(setq tab-width 4)
+	(setq python-indent 4)
 )
+(add-hook 'python-mode-hook 'my-python-mode-hook)
