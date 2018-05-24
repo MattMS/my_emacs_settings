96844e3180def21ddb87c4295ffdc05df14e0f20
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-24 14:25:35 +1000

    Add quoted insert as M-q

--- key-bindings/global.el
+++ key-bindings/global.el
@@ -5,8 +5,13 @@
 ;(define-key evil-normal-state-map (kbd "C-t") 'new-frame)
 ;(global-set-key (kbd "C-t") 'new-frame)
 
+; `C-q` is normally `quoted-insert`.
 (global-set-key (kbd "C-q") 'delete-frame)
+
 (global-set-key (kbd "C-S-q") 'save-buffers-kill-terminal)
+
+; `M-q` is normally `fill-paragraph`.
+(global-set-key (kbd "M-q") 'quoted-insert)
 
 ;(global-set-key (kbd "C-x") ctl-x-map)
 (global-set-key (kbd "M-x") 'helm-M-x)
