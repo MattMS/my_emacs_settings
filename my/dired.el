8272400b0da93b39f84d3b5d386c5c2f1b91c216
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-24 21:28:04 +1000

    Add Ranger keys to Dired

--- my/dired.el
+++ my/dired.el
@@ -1,4 +1,27 @@
+(defun my-dired-visit-parent-folder ()
+	(interactive)
+	(find-alternate-file "..")
+)
+
 (defun my-dired-mode-hook ()
+	(dired-hide-details-mode)
+
 	(define-key dired-mode-map (kbd "SPC") my-leader-map)
+
+	; `g` is normally `revert-buffer`.
+	(define-key dired-mode-map (kbd "g") 'evil-goto-first-line)
+
+	; `G` is normally `dired-do-chgrp`.
+	(define-key dired-mode-map (kbd "G") 'evil-goto-line)
+
+	; Use `dired-up-directory` to keep the buffer.
+	(define-key dired-mode-map (kbd "h") 'my-dired-visit-parent-folder)
+
+	(define-key dired-mode-map (kbd "j") 'evil-next-line)
+
+	(define-key dired-mode-map (kbd "k") 'evil-previous-line)
+
+	; Use `dired-find-file` to keep the buffer.
+	(define-key dired-mode-map (kbd "l") 'dired-find-alternate-file)
 )
 (add-hook 'dired-mode-hook 'my-dired-mode-hook)
