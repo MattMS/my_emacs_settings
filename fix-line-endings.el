46d86ed296c9e340a18d78679d4fbbb721211d4e
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-24 14:34:19 +1000

    Add fix-line-endings command

---
+++ fix-line-endings.el
@@ -0,0 +1,13 @@
+; Help from:
+; - http://ergoemacs.org/emacs/elisp_find_replace_text.html
+; - http://ergoemacs.org/emacs/keystroke_rep.html
+;
+(defun fix-line-endings ()
+	"Remove any ^M characters from line endings."
+
+	(interactive)
+	(save-excursion
+		(goto-char (point-min))
+		(while (search-forward "\r" nil t) (replace-match ""))
+	)
+)
