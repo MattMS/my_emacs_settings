a6b3e9d20f4511cc0258eb4f8cc9efef7045a79b
Matt McKellar-Spence <matt+git@mattms.com>
2018-05-24 21:49:44 +1000

    Add comment on Dired in Evil

--- my/evil.el
+++ my/evil.el
@@ -3,4 +3,6 @@
 
 (evil-mode 1)
 
+; From https://stackoverflow.com/questions/23798021/disabling-evil-mode-for-nav-in-emacs-or-any-read-only-buffers
+; Could use `(evil-set-initial-state 'dired-mode 'emacs)`.
 (add-to-list 'evil-emacs-state-modes 'dired-mode)
