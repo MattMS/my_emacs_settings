f96f81c244f333d32398f2ae42385cf280477b90
Matt McKellar-Spence <matt+git@mattms.com>
2018-06-10 16:32:29 +1000

    Update buffer switch and project find-file

--- key-bindings/leader.el
+++ key-bindings/leader.el
@@ -1,3 +1,11 @@
+; From http://emacsredux.com/blog/2013/04/28/switch-to-previous-buffer/
+(defun switch-to-previous-buffer ()
+	"Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+	(interactive)
+	(switch-to-buffer (other-buffer (current-buffer) 1))
+)
+
 ; https://github.com/noctuid/evil-guide#leader-key
 (defvar my-leader-map (make-sparse-keymap) "Keymap for \"leader key\" shortcuts.")
 
@@ -24,7 +32,9 @@
 ;
 (define-key my-leader-map "e" 'helm-find-files)
 
-(define-key my-leader-map "E" 'helm-projectile-find-file)
+; Open file-system navigator.
+;
+(define-key my-leader-map "E" 'deer)
 
 ; Open buffer switcher.
 ;
@@ -32,15 +42,32 @@
 ;
 (define-key my-leader-map "f" 'helm-buffers-list)
 
+; Open buffer manager.
+;
 (define-key my-leader-map "F" 'ibuffer)
 
 (define-key my-leader-map "l" 'avy-goto-line)
 
 (define-key my-leader-map "L" 'avy-goto-char)
 
-(define-key my-leader-map "n" 'evil-next-buffer)
+; Create a new buffer.
+;
+;(define-key my-leader-map "n" 'new-frame)
 
-(define-key my-leader-map "p" 'evil-prev-buffer)
+; Create a new frame.
+;
+; Switch to previous buffer with `evil-next-buffer`.
+;
+(define-key my-leader-map "N" 'new-frame)
+
+(define-key my-leader-map "p" 'switch-to-previous-buffer)
+
+; Search for file in project.
+;
+; Switch to previous buffer with `evil-prev-buffer`.
+;
+(define-key my-leader-map "P" 'helm-projectile-find-file)
+
 
 ; Start a Helm search in the current buffer.
 ; Requires at least 2 characters.
