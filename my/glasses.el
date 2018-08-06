b37b7c1a80a4b99ecf12d2c2dc64e2741431a3d0
Matt McKellar-Spence <matt+git@mattms.com>
2018-08-06 14:35:26 +1000

    Change glasses-mode separator and add comments

--- my/glasses.el
+++ my/glasses.el
@@ -1,15 +1,25 @@
 ; Need to run `(glasses-mode)` in mode-hooks.
 
+; Control if original separators are replaced.
+;
+; Characters matching this are replaced with `glasses-separator`.
+; Set to an empty string to avoid any replacements.
+;
 ; Default is `_`.
-; Seems to double separators when set to `_`.
-; Does nothing when `-`.
-;(setq glasses-original-separator "")
+;
+(setq glasses-original-separator "")
 
 ; Default is `_`.
-;(setq glasses-separator "_")
+;
+; - en dash: `–`
+; - em dash: `—`
+;
+(setq glasses-separator "–")
 
 ; `t` will add a space before `(`.
+;
 (setq glasses-separate-parentheses-p nil)
 
 ; non-`nil` removes separators that were added.
+;
 (setq glasses-convert-on-write-p nil)
