a954ced995b2132532097bd3596dd389487b5ba2
Matt McKellar-Spence <matt+git@mattms.com>
2018-08-21 14:40:32 +1000

    Move Helm Bookmark settings into hook

--- my/helm-bookmark.el
+++ my/helm-bookmark.el
@@ -1,3 +1,6 @@
-(setq helm-bookmark-show-location t)
+(defun my-helm-mode-hook ()
+	(setq helm-bookmark-show-location t)
 
-(define-key helm-bookmark-map (kbd "C-e") 'helm-bookmark-run-edit)
+	(define-key helm-bookmark-map (kbd "C-e") 'helm-bookmark-run-edit)
+)
+(add-hook 'helm-mode-hook 'my-helm-mode-hook)
